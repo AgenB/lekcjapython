3034fddcdc1338e8b527ce3399460aadfbe46e43
j <belicja1@gmail.com>
2019-01-25 13:54:39 +0100

    string format better

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import math
 
-print("{} + {} = {}".format(2, 3, 2+3))
+print("{:x} {:o} {:.2f}".format(13, 13, 5.56546546))
+print("{:%}".format(0.5))
+print("{:b}".format(5))
+print("{1:c} {2:c} {0:c} {1:c}".format(65, 66, 67))
