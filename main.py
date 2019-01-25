bb56cbcb37fdeac203823f9e45cfbc3da989209d
j <belicja1@gmail.com>
2019-01-25 14:03:08 +0100

    progress

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-
-print("{:x} {:o} {:.2f}".format(13, 13, 5.56546546))
-print("{:%}".format(0.5))
-print("{:b}".format(5))
-print("{1:c} {2:c} {0:c} {1:c}".format(65, 66, 67))
+a = float(input("Podaj a = "))
+b = float(input("Podaj b = "))
+print("{:.1f}".format((a**2 + b**2)**0.5))
