24847134f5ade1b5d1a71c45d236acdeb198172a
j <belicja1@gmail.com>
2019-01-25 13:43:54 +0100

    pi precission

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-ile = int(input()) // 2 + 1
-wyraz = input()
-for i in range(ile):
-    print("."*(ile - i) + wyraz[ile - i:ile + i + 1] + "."*(ile - i))
+import math
+
+for i in range(11):
+    print(("%."+str(i)+"f") % math.pi)
