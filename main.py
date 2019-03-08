f787d08d2e25cc9b043dfea773fcb719bbd9d9b6
j <belicja1@gmail.com>
2019-03-08 13:41:40 +0100

    functions

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import re
-s = "gdfgd89.25.166.111gdfgfd"
-w = re.search(r"[0-9A-Fa-f]+", s)
-print(w)
+def silnia(n):
+    s = 1
+    for i in range(1, n+1):
+        s *= i
+    return s
+
+def silniaR(n):
+    if (n == 0):
+        return 1
+    else:
+        return n * silniaR(n - 1)
+
+def nwd(a, b):
+    while (b != 0):
+        a, b = b, a % b
+    return a
+
+print(nwd(12,18))
