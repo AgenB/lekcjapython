010d36cb4a088900b3e63734310180895f961caa
j <belicja1@gmail.com>
2019-03-08 14:04:25 +0100

    stuff from spoj

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-def silnia(n):
-    s = 1
-    for i in range(1, n+1):
-        s *= i
-    return s
-
-def silniaR(n):
-    if (n == 0):
-        return 1
-    else:
-        return n * silniaR(n - 1)
-
 def nwd(a, b):
     while (b != 0):
         a, b = b, a % b
     return a
 
-print(nwd(12,18))
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    print(nwd(a, b))
