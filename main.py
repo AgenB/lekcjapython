5b820eec3c458399041956e4a6b451e11e97f57c
j <belicja1@gmail.com>
2019-03-15 13:29:35 +0100

    lambda functions

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-def nwd(a, b):
-    while (b != 0):
-        a, b = b, a % b
-    return a
-
-def nwdMul(a, b, *c):
-    d = nwd(a, b)
-    for i in c:
-        d = nwd(d, i)
-    return d
-
-print(nwdMul(12, 18, 33, 60))
+f = lambda x : x**2
+print(f(3))
