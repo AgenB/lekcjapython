ef2541b914e98280f5f761edef1b190e5a18fccb
j <belicja1@gmail.com>
2019-03-22 14:02:46 +0100

    program for spoj v2

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 d = {}
 for _ in range(int(input())):
-    for i in input():
-        # if(i != " "):
-        d[i] = d.get(i, 0) + 1
-a = list(d.items())
+    for i in input(): d[i] = d.get(i, 0) + 1
+a = (list(d.items())
 a.sort(key=lambda x: x[0].swapcase())
-for i in a[1:]:
-    print(i[0], i[1])
+for i in a[1:]: print(i[0], i[1])
