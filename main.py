8d0690ebc5590a4010eba66e9458983329e4c340
j <belicja1@gmail.com>
2019-03-22 12:52:44 +0100

    dictionary stuff

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ a = {"ala": 15, "janek": 20}
 a[4] = 5
 a[(2, 3, 4)] = "krotka"
 print(a)
+print(a.items())
+print(a.keys())
+print(a.values())
+print("ala" in a)
+print(a.get("aa", "nie ma"))
+print(a.get("ala", "nie ma"))
