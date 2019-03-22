37ab53b44fa8dc49428ce2bd8ba1f9b9b44e3cd4
j <belicja1@gmail.com>
2019-03-22 13:00:15 +0100

    smol code

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-a = {"ala": 15, "janek": 20}
-a[4] = 5
-a[(2, 3, 4)] = "krotka"
-print(a)
-print(a.items())
-print(a.keys())
-print(a.values())
-print("ala" in a)
-print(a.get("aa", "nie ma"))
-print(a.get("ala", "nie ma"))
+d = {}
+s = input()
+for i in s:
+    d[i] = i
+print(len(d))
