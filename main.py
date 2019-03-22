7276cb18dbf7f82faa6f7be7066bb5442b2c6a54
j <belicja1@gmail.com>
2019-03-22 13:38:29 +0100

    letter rank in string

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 d = {}
 s = input()
+m = s[0]
 for i in s:
-    d[i] = i
-print(len(d))
+    d[i] = d.get(i, 0) + 1
+    if(d[i] > d[m]):
+        m = i
+print(d)
+print(m + ", " + str(d[m]))
+print("\n")
+a = list(d.items())
+a.sort(key = lambda k: k[1], reverse = True)
+print(a)
+print(a[0])
