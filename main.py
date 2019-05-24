96b0052998a43d98e94badf71afb6b65a62ce74b
j <belicja1@gmail.com>
2019-05-24 13:45:19 +0200

    try except

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-class Osoba:
-    def __init__(self, imie, nazw, wiek):
-        self.imie = imie
-        self.nazw = nazw
-        self.wiek = wiek
-    def __del__(self):
-        print(f"{self.imie} {self.nazw} zmarł.")
-    def __str__(self):
-        return f"Nazywam się {self.imie} {self.nazw}, mam {self.wiek} lat."
-    def przedstaw(self):
-        print(f"Nazywam się {self.imie} {self.nazw}, mam {self.wiek} lat.")
-    def urodziny(self):
-        self.wiek += 1
-        self.przedstaw()
-
-class Prostokat:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-    def __str__(self):
-        return f"O[{self.obwod()}] P[{self.pole()}] D[{self.przek()}]"
-    def obwod(self):
-        return self.a*2 + self.b*2
-    def pole(self):
-        return self.a * self.b
-    def przek(self):
-        return (self.a**2 + self.b**2)**0.5
-
-ktos = Osoba("Jan", "Kowalski", 16)
-k2 = ktos
-ktos = 0
-x = Prostokat(3, 4)
-print(x)
+try:
+    x = int(input())
+    print(1 / x)
+except ZeroDivisionError:
+    print("Nie wolno dzielić przez 0")
+except ValueError:
+    print("Miałeś podać liczbę całkowitą 😣 ")
+except Exception as e:
+    print("Coś poszło nie tak 😣 ", type(e))
