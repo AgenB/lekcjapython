4e110cf190d966ac8a6cbd0c6e8db253c9bc3a60
j <belicja1@gmail.com>
2019-06-07 13:50:05 +0200

    finished chessboard

--- main.py
+++ main.py
@@ -13,19 +13,21 @@ def poly(n, d, color):
         t.left(360/n)
     t.end_fill()
 
-color1 = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-color2 = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+def chess(x, y, l, c1, c2):
+    for i in range(y):
+        for j in range(x):
+            if ((i + j) % 2 == 0):
+                poly(4, l/x, c1)
+            else:
+                poly(4, l/x, c2)
+            t.forward(l/x)
+        t.pu()
+        t.backward(l)
+        t.right(90)
+        t.forward(l/x)
+        t.left(90)
+        t.pd()
 
-for i in range(8):
-    for j in range(8):
-        if ()
-        poly(4, 10, color1)
-        t.forward(10)
-    t.pu()
-    t.backward(80)
-    t.right(90)
-    t.forward(10)
-    t.left(90)
-    t.pd()
+chess(8, 8, 120, (random.randint(0,255),random.randint(0,255),random.randint(0,255)), (random.randint(0,255),random.randint(0,255),random.randint(0,255)))
 
 input()
